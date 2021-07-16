524dbf037bdf1b57b391649711501b23100fe147
gabei <gabe1792@gmail.com>
2021-07-16 15:04:52 -0500

    added basic calculator class setup

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,29 @@
 console.log('script.js loaded...');
+
+
+
+
+
+
+/* Calculator
+___________________________*/
+class Calculator {
+  //empty for now
+  constructor(){}
+
+  multiply(a, b){
+    return a * b;
+  }
+
+  divide(a, b){
+    return a / b
+  }
+
+  add(a, b){
+    return a + b;
+  }
+
+  subtract(a, b){
+    return a - b;
+  }
+}
