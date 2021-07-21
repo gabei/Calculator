5c49f7345797369a6259228b253976e3771dd9dd
gabei <gabe1792@gmail.com>
2021-07-21 18:00:15 -0500

    added functionality associated with getting input from the display to the calculator and setting up operations

--- scripts/script.js
+++ scripts/script.js
@@ -42,3 +42,55 @@ function updateDisplay(value){
   calDisplay.textContent = '' :
   calDisplay.textContent = calDisplay.textContent + value;
 }
+
+/* Input Operation
+___________________________*/
+let operands = [];
+let operator = [];
+
+function getOperandFromInput(){
+  let newOperand = calDisplay.textContent;
+  newOperand = stringToNum(newOperand);
+  operands.push(newOperand);
+}
+
+function stringToNum(string){
+  return --string;
+}
+
+function clearDisplay(){
+  calDisplay.textContent = '';
+}
+
+function isTimeToEvaluate(){
+  return (
+    operands.length === 2 &&
+    operator.length > 0
+  )
+}
+
+/* Calculator Operations
+___________________________*/
+let calculator = new Calculator();
+
+function operate(a, b, operator){
+  let result;
+
+  switch(operator){
+    case '*':
+      result = calculator.multiply(a, b);
+      break;
+    case '/':
+      result = calculator.divide(a, b);
+      break;
+    case '+':
+      result = calculator.add(a, b);
+      break;
+    case '-':
+      result = calculator.subtract(a, b);
+      break;
+    default:
+      break;
+  }
+  return result;
+}
